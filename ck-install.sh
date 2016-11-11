f5932ff093f498ac76873cb5335c0a01f7841067
Carson Killbreath <kill4992@kettering.edu>
2016-11-11 14:02:51 -0500

    Significant number of changes, mostly for handling user argument.

--- ck-install.sh
+++ ck-install.sh
@@ -1,70 +1,103 @@
+#!/bin/bash
+
+USER=$1
+HOME=/home/$USER
+
+if [[ ! -d $HOME ]]; then
+    echo "User does not have a home directory."
+    exit
+fi
+
 GITHUB_USERNAME=carsonk
 GITHUB_REPO=git@github.com:carsonk/dotfiles.git
 
+echo "Home is $HOME"
+echo ~
+
+runu="sudo -u $USER"
+
 # Necessary applications for this script.
-sudo apt-get install -y git vim zsh
+sudo apt-get install -y git vim zsh csh
 
 # Let's do the things that require prompts up front.
-ssh-keygen -f $HOME/.ssh/id_rsa
+$runu mkdir $HOME/.ssh
+$runu ssh-keygen -f $HOME/.ssh/id_rsa
 
 # Install oh-my-zsh and switch to zsh shell.
 echo "[*] Installing oh-my-zsh."
-sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-csh -s `which zsh`
+sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | $runu zsh
+chsh -s /bin/zsh $USER
 
 # Install zplug.
 echo "[*] Installing zplug."
-curl -sL zplug.sh/installer | zsh
+$runu curl -sL zplug.sh/installer | $runu zsh
 
 # Install ssh-keyreg and run.
 echo "[*] Running ssh-keyreg to add key to github account."
 sudo sh -c "curl https://raw.githubusercontent.com/b4b4r07/ssh-keyreg/master/bin/ssh-keyreg -o /usr/local/bin/ssh-keyreg && chmod +x /usr/local/bin/ssh-keyreg"
-ssh-keyreg -u $GITHUB_USERNAME --path id_rsa.pub github
+$runu ssh-keyreg -u $GITHUB_USERNAME --path id_rsa.pub github
 
 # Configure dot repo.
 echo "[*] Configuring dotfiles repo."
+$runu touch .gitignore
 echo ".cfg" >> .gitignore
-git clone --bare $GITHUB_REPO $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+$runu git clone --bare $GITHUB_REPO $HOME/.cfg
+config="$runu /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 # Make backup of initial dotfiles that might be overwritten.
-mkdir -p .config-backup && \
-config checkout
-if [$? = 0]; then
+$runu mkdir -p .config-backup && \
+$config checkout
+if [ $? = 0 ]; then
     echo "[*] Checkout out config success."
 else
     echo "[*] Backing up existing dot files."
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    $config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} $runu mv {} .config-backup/{}
 fi;
-config checkout
-config config --local status.showUntrackedFiles no
+$config checkout
+$config config --local status.showUntrackedFiles no
 
 echo "[*] Pulling down Vim Pathogen."
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+$runu mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+$runu curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim
-git init
-git submodule add https://github.com/flazz/vim-colorschemes.git bundle/colorschemes
+$runu git init
+$runu git submodule add https://github.com/flazz/vim-colorschemes.git bundle/colorschemes
+
+# In case someone doesn't get the message.
+chown $USER:$USER $HOME/*
 
 # Install applications.
 echo "[*] Installing apt-get applications."
-sudo apt-get install -y python3 python3-pip bpython3 python-pip
-sudo pip install --upgrade pip
-sudo pip3 install --upgrade pip
+apt-get install -y python3 python3-pip bpython3 python-pip atom sqlite3 mysql
+pip install --upgrade pip
+pip3 install --upgrade pip
 
 echo "[*] Theme shit."
-sudo add-apt-repository -y ppa:noobslab/themes
-sudo add-apt-repository -y ppa:noobslab/icons
-sudo apt-get update
-sudo apt-get install -y unity-tweak-tool flatabulous-theme ultra-flat-icons
+add-apt-repository -y ppa:noobslab/themes
+add-apt-repository -y ppa:noobslab/icons
+apt-get update
+apt-get install -y unity-tweak-tool flatabulous-theme ultra-flat-icons
 
 echo "[*] Spotify."
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install -y spotify-client
+apt-get update
+apt-get install -y spotify-client
 
 echo "[*] Stuff we're gonna need for Android Studio."
-sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
 
-echo "[*] Complete."
+echo "[*] Installing chrome."
+apt-get install -y libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome*.deb
+if [ $? -eq 0 ]; then
+    echo "[*] Installed chrome succesfully."
+else
+    echo "[*] Attempting to install Chrome deps."
+    apt install -f
+    dpkg -i google-chrome*.deb
+fi        
+
+echo "[*] Complete. Reboot to complete."
+
